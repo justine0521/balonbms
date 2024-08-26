0c8a927a523b2499cf916656eba23463ea63a5e7
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-08-26 20:17:38 +0800

    Edit route from /pages/services to /pages/service

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CertificateForSoloParent from './Request-Certificate/CertificateForSoloPa
 
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Services from './Pages/Services';
+import Service from './Pages/Service';
 import TrackRequest from './Pages/TrackRequest';
 import Faq from './Pages/Faq';
 import Location from './Pages/Location';
@@ -34,19 +34,19 @@ function App() {
             <Route path="/" element={<Home />} />
 
             <Route path="/barangay-clearance-form" element={<BarangayClearance />} />
-            <Route path='/certificate-of-residency-form' element={<CertificateOfResidency />}/>
-            <Route path='/certificate-of-indigency-form' element={<CertificateOfIndigency />}/>
-            <Route path='/certificate-of-low-income-form' element={<CertificateOfLowIncome />}/>
-            <Route path='/business-clearance-form' element={<BusinessClearance />}/>
-            <Route path='/certificate-of-death-form' element={<CertificateOfDeath />}/>
-            <Route path='/certificate-of-no-property-form' element={<CertificateOfNoProperty />}/>
-            <Route path='/certificate-of-good-moral-form' element={<CertificateOfGoodMoral />}/>
-            <Route path='/certificate-for-new-residents-form' element={<CertificateForNewResidents />}/>
-            <Route path='/certificate-for-solo-parent-form' element={<CertificateForSoloParent />}/>
+            <Route path='/certificate-of-residency-form' element={<CertificateOfResidency />} />
+            <Route path='/certificate-of-indigency-form' element={<CertificateOfIndigency />} />
+            <Route path='/certificate-of-low-income-form' element={<CertificateOfLowIncome />} />
+            <Route path='/business-clearance-form' element={<BusinessClearance />} />
+            <Route path='/certificate-of-death-form' element={<CertificateOfDeath />} />
+            <Route path='/certificate-of-no-property-form' element={<CertificateOfNoProperty />} />
+            <Route path='/certificate-of-good-moral-form' element={<CertificateOfGoodMoral />} />
+            <Route path='/certificate-for-new-residents-form' element={<CertificateForNewResidents />} />
+            <Route path='/certificate-for-solo-parent-form' element={<CertificateForSoloParent />} />
 
             <Route path='/pages/home' element={<Home />} />
             <Route path='/pages/about' element={<About />} />
-            <Route path='/pages/services' element={<Services />} />
+            <Route path='/pages/service' element={<Service />} />
             <Route path='/pages/track-request' element={<TrackRequest />} />
             <Route path='/pages/faq' element={<Faq />} />
 
