03e45cebaf8a3e885daab9ccd6d50216a3cb1985
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-09-02 01:40:41 +0800

    improve the layout of announcement page

--- src/Pages/Announcement.jsx
+++ src/Pages/Announcement.jsx
@@ -2,12 +2,11 @@ import React, { useState, useRef, useEffect } from 'react';
 import AnnouncementModal from '../Modal/AnnouncementModal';
 
 import DefaultPicture from '../Images/Logo.png';
-import Hotspring from '../Images/Hotspring.jpg'
+import Hotspring from '../Images/Hotspring.jpg';
 import Picture from '../Images/About-Picture/bg-Picture.jpg';
 
 function Announcement() {
-
-  const [isAnnouncement, SetIsAnnouncement] = useState(true)
+  const [isAnnouncement, SetIsAnnouncement] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isOverflowing, setIsOverflowing] = useState(false);
   const textRef = useRef(null);
@@ -26,10 +25,42 @@ function Announcement() {
     return () => window.removeEventListener('resize', checkOverflow);
   }, []);
 
+  // Sample announcement data
+  const announcements = [
+    {
+      title: 'Bigayan ng ayuda',
+      content:
+        'Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?',
+      date: 'September 2, 2024',
+      image: Hotspring,
+    },
+    {
+      title: 'Road Repair Advisory',
+      content:
+        'Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?',
+      date: 'September 1, 2024',
+      image: Hotspring,
+    },
+    {
+      title: 'Road Repair Advisory',
+      content:
+        'Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?',
+      date: 'September 1, 2024',
+      image: Hotspring,
+    },
+    {
+      title: 'Road Repair Advisory',
+      content:
+        'Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?',
+      date: 'September 1, 2024',
+      image: Hotspring,
+    },
+  ];
+
   return (
     <section>
       <div className='relative h-96 w-full bg-cover bg-center' style={{ backgroundImage: `url(${Picture})` }}>
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className='absolute inset-0 bg-black bg-opacity-50'></div>
 
         <div className="relative flex justify-center items-center h-full z-10 p-4 text-white">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-20">ANNOUNCEMENT</h1>
@@ -43,44 +74,36 @@ function Announcement() {
       )}
 
       {isAnnouncement && (
-        <div className='flex flex-wrap justify-center gap-10 h-fit m-8 mx-10'>
-          <div className='flex flex-wrap md:flex-nowrap justify-center relative rounded shadow-xl drop-shadow-xl'>
-            <img src={Hotspring} alt="Hotspring" className='w-full md:w-96 h-56 object-fit rounded-t-lg md:rounded-r-none md:rounded-l-lg' />
-
-            <div className='w-full md:w-[450px] px-5 mt-5'>
-              <h1 className='text-lg text-green-500 font-semibold'>Bigayan ng ayuda</h1>
-
-              <p ref={textRef} className='text-sm  text-gray-500 overflow-hidden overflow-ellipsis line-clamp-6'>
-                Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?
-                Ut atque corporis veritatis corrupti facere ab odit nulla ducimus suscipit? Nihil, veritatis eligendi? Aliquam rerum culpa distinctio ratione enim, doloribus nostrum voluptate ea, laborum eaque veniam inventore porro error?
-              </p>
-
-              {isOverflowing && (
-                <button onClick={() => setIsModalOpen(true)} className='flex items-center text-sm float-end p-2 mt-2 text-green-500 hover:underline'>
-                  Read More
-                </button>
-              )}
+        <div className='flex flex-wrap justify-center gap-6 p-6'>
+          {announcements.map((announcement, index) => (
+            <div
+              key={index}
+              className='flex w-full md:w-[500px] h-48 bg-white rounded-lg shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105'
+            >
+              <img
+                src={announcement.image}
+                alt={announcement.title}
+                className='w-48 h-full object-cover'
+              />
+              <div className='flex flex-col justify-between p-4'>
+                <div>
+                  <h2 className='text-md font-semibold text-green-500'>{announcement.title}</h2>
+                  <p className='text-xs text-gray-400'>Posted on: {announcement.date}</p>
+                </div>
+                <p ref={textRef} className='text-sm text-gray-500 overflow-hidden overflow-ellipsis line-clamp-3'>
+                  {announcement.content}
+                </p>
+                {isOverflowing && (
+                  <button
+                    onClick={() => setIsModalOpen(true)}
+                    className='text-sm text-green-500 hover:underline mt-2 self-start'
+                  >
+                    Read More
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-
-          <div className='flex flex-wrap md:flex-nowrap justify-center relative rounded shadow-xl drop-shadow-xl'>
-            <img src={Hotspring} alt="Hotspring" className='w-full md:w-96 h-56 object-fit rounded-t-lg md:rounded-r-none md:rounded-l-lg' />
-
-            <div className='w-full md:w-[450px] px-5 mt-5'>
-              <h1 className='text-lg text-green-500 font-semibold'>Road Repair Advisory</h1>
-
-              <p ref={textRef} className='text-sm  text-gray-500 overflow-hidden overflow-ellipsis line-clamp-6'>
-                Bukas po ay bigayan ng ayuda simula 9AM to 12PM. Maaring dalhin po ang mga sumusunod na requirements 1 valid ID, limang manok, limang baka at limang kambing at sabay sabay natin lutuin. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, maxime! Nobis modi accusamus veniam beatae ex consectetur voluptatibus laboriosam libero assumenda id, quo sed, vitae, laborum optio hic cum voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur tempore autem sint. Assumenda autem dolore totam sequi voluptas voluptates consectetur laudantium unde laborum, fuga dolores temporibus beatae quo ipsum quia?
-                Ut atque corporis veritatis corrupti facere ab odit nulla ducimus suscipit? Nihil, veritatis eligendi? Aliquam rerum culpa distinctio ratione enim, doloribus nostrum voluptate ea, laborum eaque veniam inventore porro error?
-              </p>
-
-              {isOverflowing && (
-                <button onClick={() => setIsModalOpen(true)} className='flex items-center text-sm float-end p-2 mt-2 text-green-500 hover:underline'>
-                  Read More
-                </button>
-              )}
-            </div>
-          </div>
+          ))}
         </div>
       )}
 
