55cd6e245301aa3542b67645d8725c54a5fe65e9
JustineSantos <justine0521>
2024-09-15 11:09:32 +0800

    edit AnnouncementModal, edit width of the logo in services.jsx

--- src/Modal/AnnouncementModal.jsx
+++ src/Modal/AnnouncementModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 import { BsFullscreen, BsFullscreenExit } from 'react-icons/bs';
 import '../App.css'; // Assuming you have global styles in App.css
@@ -7,81 +7,46 @@ import { format } from 'date-fns';
 import Hotspring from '../Images/Hotspring.jpg';
 
 function AnnouncementModal({ isOpen, onClose, announcement }) {
-    const imageRef = useRef(null); // Reference to the image
-    const [isFullscreen, setIsFullscreen] = useState(false); 
+    const [isFullscreen, setIsFullscreen] = useState(false);
 
-    const handleFullscreen = () => {
-        if (imageRef.current) {
-            if (imageRef.current.requestFullscreen) {
-                imageRef.current.requestFullscreen();
-            } else if (imageRef.current.webkitRequestFullscreen) {
-                imageRef.current.webkitRequestFullscreen();
-            } else if (imageRef.current.msRequestFullscreen) {
-                imageRef.current.msRequestFullscreen();
-            }
-        }
+    const handleImageFullscreen = () => {
+        setIsFullscreen(!isFullscreen);
     };
-
-    const handleExitFullscreen = () => {
-        if (document.fullscreenElement) {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-        }
-    };
-
-    useEffect(() => {
-        const handleFullscreenChange = () => {
-            setIsFullscreen(!!document.fullscreenElement);
-        };
-
-        document.addEventListener('fullscreenchange', handleFullscreenChange);
-
-        return () => {
-            document.removeEventListener('fullscreenchange', handleFullscreenChange);
-        };
-    }, []);
 
     if (!isOpen || !announcement) return null;
 
     return (
         <section className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-60 z-50 overflow-auto">
-            <div className="relative bg-white w-[90%] max-w-[750px] max-h-[90%] m-4 rounded-2xl shadow-xl overflow-y-auto no-scrollbar">
+            <div className={`relative bg-white w-[90%] max-w-[750px] max-h-[90%] m-4 rounded-2xl shadow-xl overflow-y-auto no-scrollbar transition-all duration-300`}>
                 <div className="relative">
                     {/* Header Image */}
                     <img
-                        ref={imageRef} // Attach the ref to the image element
                         src={announcement.imageUrl || Hotspring}
                         alt={announcement.title}
-                        className={`w-full h-96 rounded-t-2xl ${isFullscreen ? 'object-contain' : 'object-cover'}`}
+                        className={`w-full h-96 object-cover rounded-t-2xl transition-all duration-300`}
                     />
 
-                    {!isFullscreen && (
-                        <button onClick={handleFullscreen} className="absolute top-4 left-4 text-blue-500 bg-white rounded-full p-2 shadow-md transition-all duration-300 hover:bg-blue-500 hover:text-white focus:outline-none">
-                            <BsFullscreen size={20} />
-                        </button>
-                    )}
+                    <button onClick={handleImageFullscreen} className="absolute top-4 left-4 text-xl text-blue-500 bg-white rounded-full p-2 shadow-md transition-all duration-300 hover:bg-blue-500 hover:text-white focus:outline-none">
+                        {isFullscreen ? <BsFullscreenExit /> : <BsFullscreen />}
+                    </button>
 
                     {isFullscreen && (
-                        <button onClick={handleExitFullscreen} className="fixed top-4 right-4 z-50 text-red-500 bg-white rounded-full p-2 shadow-md transition-all duration-300 hover:bg-red-500 hover:text-white focus:outline-none">
-                            <BsFullscreenExit size={20} />
-                        </button>
+                        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center p-10 z-50" onClick={handleImageFullscreen}>
+                            <img src={announcement.imageUrl || Hotspring} alt={announcement.title} className="w-auto h-auto max-h-full max-w-full" />
+                        </div>
                     )}
 
-                    {!isFullscreen && (
-                        <button onClick={onClose} className="fixed top-4 right-4 text-red-500 bg-white rounded-full p-2 shadow-md transition-all duration-300 hover:bg-red-500 hover:text-white focus:outline-none" >
-                            <IoMdClose size={20} />
-                        </button>
-                    )}
+                    <button
+                        onClick={onClose}
+                        className="fixed top-4 right-4 text-red-500 bg-white rounded-full p-2 shadow-md transition-all duration-300 hover:bg-red-500 hover:text-white focus:outline-none"
+                    >
+                        <IoMdClose size={20} />
+                    </button>
                 </div>
 
                 <div className="w-full p-6">
                     <h1 className="text-2xl text-green-600 font-bold">{announcement.title}</h1>
-                    <p className='text-xs text-gray-400 mb-4'>Posted on: {format(new Date(announcement.createdAt), 'Pp')}</p>
+                    <p className="text-xs text-gray-400 mb-4">Posted on: {format(new Date(announcement.createdAt), 'Pp')}</p>
                     <hr />
                     <br />
                     <div
