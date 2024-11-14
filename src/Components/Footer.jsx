d2fd903485eebf51591310f64dfda46f75093300
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-11-14 11:24:59 +0800

    Update the footer into grid system

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -36,69 +36,73 @@ function Footer() {
   }, [])
 
   return (
-    <footer>
-      <div className="footer bg-green-500 p-10 text-white">
+    <footer className="bg-green-500">
+      <div className="footer max-w-screen-xl mx-auto p-10 text-white grid gap-8 lg:grid-cols-4 sm:grid-cols-1">
         <aside>
-          <div className='flex items-center gap-2'>
-            <img src={barangayLogo} alt="Balon Anito Logo" className='h-24' />
-
-            <div className='text-xl text-white'>
+          <div className="flex flex-wrap items-center gap-2">
+            <img src={barangayLogo} alt="Balon Anito Logo" className="h-24" />
+            <div className="text-xl text-white">
               <p>Barangay</p>
               <p>{barangayName}</p>
             </div>
           </div>
-
-          <p className='font-dancing text-xl text-white sm:w-full md:w-3/4'>Maayos na Serbisyo Publiko, Susi sa Progresibong Balon Anito</p>
+          <p className="font-dancing text-xl text-white sm:w-full md:w-3/4">
+            Maayos na Serbisyo Publiko, Susi sa Progresibong Balon Anito
+          </p>
         </aside>
 
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover"><NavLink to={'/barangay-clearance-form'}>Barangay Clearance</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/business-clearance-form'}>Business Clearance</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/travel-permit-form'}>Travel Permit</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/job-seeker-form'}>First Time Job Seeker</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/common-law-form'}>Certificate of Common Law</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/guardianship-form'}>Certificate of Guardianship</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/certificate-of-indigency-form'}>Certificate of Indigency</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/certificate-of-residency-form'}>Certificate of Residency</NavLink></a>
-          <a className="link link-hover" href=''><NavLink to={'/certificate-for-solo-parent-form'}>Certificate of Solo Parent</NavLink></a>
+        <nav className="lg:mx-auto">
+          <h6 className="text-xl uppercase font-bold text-green-100">Services</h6>
+          <div className="grid gap-2 lg:grid-cols-2 grid-cols-1">
+            <div className="flex flex-col gap-2">
+              <NavLink to={'/barangay-clearance-form'} className="link link-hover">Barangay Clearance</NavLink>
+              <NavLink to={'/business-clearance-form'} className="link link-hover">Business Clearance</NavLink>
+              <NavLink to={'/travel-permit-form'} className="link link-hover">Travel Permit</NavLink>
+              <NavLink to={'/job-seeker-form'} className="link link-hover">First Time Job Seeker</NavLink>
 
+            </div>
+            <div className="flex flex-col gap-2 w-[175px]">
+              <NavLink to={'/certificate-of-indigency-form'} className="link link-hover">Certificate of Indigency</NavLink>
+              <NavLink to={'/certificate-of-residency-form'} className="link link-hover">Certificate of Residency</NavLink>
+              <NavLink to={'/certificate-for-solo-parent-form'} className="link link-hover">Certificate of Solo Parent</NavLink>
+              <NavLink to={'/common-law-form'} className="link link-hover">Certificate of Common Law</NavLink>
+              <NavLink to={'/guardianship-form'} className="link link-hover">Certificate of Guardianship</NavLink>
+            </div>
+          </div>
         </nav>
 
-        <nav>
-          <h6 className="footer-title">Quik Links</h6>
-          <a className="link link-hover"><NavLink to={"/"}>Home</NavLink></a>
-          <a className="link link-hover"><NavLink to={"/pages/about"}>About</NavLink></a>
-          <a className="link link-hover"><NavLink to={"/pages/service"}>Service</NavLink></a>
-          <a className="link link-hover"><NavLink to={"/pages/news"}>News</NavLink></a>
-          <a className="link link-hover"><NavLink to={"/pages/faq"}>FAQ</NavLink></a>
-          <a className="link link-hover"><NavLink to={"/pages/track-request"}>Track Request</NavLink></a>
+        <nav className="lg:mx-auto">
+          <h6 className="text-xl uppercase font-bold text-green-100">Quick Links</h6>
+          <NavLink to={"/"} className="link link-hover">Home</NavLink>
+          <NavLink to={"/pages/about"} className="link link-hover">About</NavLink>
+          <NavLink to={"/pages/service"} className="link link-hover">Services</NavLink>
+          <NavLink to={"/pages/news"} className="link link-hover">News</NavLink>
+          <NavLink to={"/pages/faq"} className="link link-hover">FAQ</NavLink>
+          <NavLink to={"/pages/track-request"} className="link link-hover">Track Request</NavLink>
         </nav>
 
-        <nav>
-          <h6 className="footer-title">Contact Us</h6>
-
-          <li className='flex items-center gap-2'>
-            <FaPhoneAlt className='text-white text-xl' />
+        <nav className="lg:mx-auto">
+          <h6 className="text-xl uppercase font-bold text-green-100">Contact Us</h6>
+          <li className="flex items-center gap-2">
+            <FaPhoneAlt className="text-white text-xl" />
             {contactNumber}
           </li>
-
-          <a href="https://www.facebook.com/profile.php?id=100092042062918" className='flex items-center gap-2 link link-hover'>
-            <FaFacebookF className='text-white text-xl' />
+          <a href="https://www.facebook.com/profile.php?id=100092042062918" className="flex items-center gap-2 link link-hover">
+            <FaFacebookF className="text-white text-xl" />
             {facebook}
           </a>
-          <a href="mailto:betterbalonanito@gmail.com" className='flex items-center gap-2 link link-hover'>
-            <SiGmail className='text-white text-xl' />
+          <a href="mailto:betterbalonanito@gmail.com" className="flex items-center gap-2 link link-hover">
+            <SiGmail className="text-white text-xl" />
             {email}
           </a>
         </nav>
       </div>
 
-      <div className="border-t border-white bg-green-500  p-8 text-center">
-        <p className='text-white'><strong>&copy;</strong> {new Date().getFullYear()} <strong className="text-white">Balon Anito.</strong> All rights reserved.</p>
+      <div className="border-t border-white bg-green-500 p-8 text-center">
+        <p className="text-white"><strong>&copy;</strong> {new Date().getFullYear()} <strong className="text-white">Balon Anito.</strong> All rights reserved.</p>
       </div>
-
     </footer>
+
   );
 }
 
